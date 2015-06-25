9021e6205edbfb6ad21915170c6846a4b024a9d8
Danny Coenen <info@dannycoenen.nl>
2015-06-25 20:55:12 +0200

    Eatable elements
    ADD: eatable elements
    ADD: pixel can grow in size while eating elements

--- pixel/script/jquery.js
+++ pixel/script/jquery.js
@@ -3,48 +3,48 @@ $(document).ready( function() {
 	var wHeight = $(document).height(); 
 	var wWidth = $(document).width();
 	
-	setInterval(function() {animatePixel('.px_1', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_2', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_3', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_4', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_5', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_6', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_7', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_8', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_9', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_10', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_11', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_12', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_13', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_14', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_15', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_16', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_17', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_18', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_19', wHeight, wWidth);}, speed());
-	setInterval(function() {animatePixel('.px_20', wHeight, wWidth);}, speed());
-	
+	setInterval(function() {animatePixel('.px_1', wHeight, wWidth);}, speed('fixed'));
+	setInterval(function() {spawnElement(wHeight, wWidth)}, 100);
+
 });
 
 /************************************************
  set speed
 ************************************************/
-function speed(){
-	return Math.floor((Math.random() * 50) + 1);
+function speed(mode){
+	if(mode == 'fixed'){
+		return 1;
+	}else if(mode == 'random'){
+		return Math.floor((Math.random() * 50) + 1);
+	}
+}
+
+/************************************************
+ spawn pixel elements
+************************************************/
+function spawnElement(wHeight, wWidth){
+	if($('.element').length !== 50){
+		var coodTop = Math.floor((Math.random() * wHeight) + 1) + 'px';
+		var coodLeft = Math.floor((Math.random() * wWidth) + 1) + 'px';
+		$('<div style="top:' + coodTop + '; left:' + coodLeft + ';" class="element el_1"></div>').hide().appendTo('body').fadeIn(1000);
+		//$('<div style="top:342px; left:320px;" class="element el_1"></div>').appendTo('body');
+	}
 }
 
 /************************************************
  pixel brains
 ************************************************/
 function animatePixel(px, wHeight, wWidth){
-	var pixel = $(px);
-	var offset = pixel.offset();
+	var pixel = $(px).offset();
+	var size = $(px).height();
 	
 	// animate height
-	if(offset.top == wHeight - 50){
+	if(pixel.top >= wHeight - size){
 		localStorage.setItem("dirH" + px, "min");
-	}else if(offset.top == 0){
+		console.log('i am going up');
+	}else if(pixel.top <= 0){
 		localStorage.setItem("dirH" + px, "plus");
+		console.log('i am going down');
 	}
 	if(localStorage.getItem("dirH" + px) == "min"){
 		$(px).animate({top: "-=1"}, 0);
@@ -53,14 +53,29 @@ function animatePixel(px, wHeight, wWidth){
 	}
 	
 	// animate width
-	if(offset.left == wWidth - 50){
+	if(pixel.left >= wWidth - size){
 		localStorage.setItem("dirW" + px, "min");
-	}else if(offset.left == 0){
+		console.log('i am going left');
+	}else if(pixel.left <= 0){
 		localStorage.setItem("dirW" + px, "plus");
+		console.log('i am going right');
 	}
 	if(localStorage.getItem("dirW" + px) == "min"){
 		$(px).animate({left: "-=1"}, 0);
 	}else if(localStorage.getItem("dirW" + px) == "plus"){
 		$(px).animate({left: "+=1"}, 0);
 	}
+	
+	// eat element
+	var element = '';
+	
+	$('.element').each(function(index) {
+		element = $(this).offset();
+		if(element.top >= pixel.top && element.top <= pixel.top + size && element.left >= pixel.left && element.left <= pixel.left + size){
+			$(this).remove();
+			$(px).animate({width: "+=1", height: "+=1"}, 0);
+			console.log('element ' + index + ': has been eaten');
+		}
+	});
+
 }
